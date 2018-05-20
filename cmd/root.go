4e029f9e912fa2e7f4b6260f7820ccb0813e357c
Dan Wendorf <dan.wendorf@opendoor.com>
2018-05-20 16:15:09 -0700

    Display version with `--version`

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,32 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
+
+func init() {
+	rootCmd.Flags().BoolP("version", "", false, "Display version")
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "dory",
 	Short: "Dory is a very forgetful fish",
 	Long:  `A simple store for data you want to save for a short bit, like a password during your work day.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		version, err := cmd.Flags().GetBool("version")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if version {
+			fmt.Println("0.0.1")
+			os.Exit(0)
+		}
+
 		cmd.Help()
 	},
 }
