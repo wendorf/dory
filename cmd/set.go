4d65cc3ff822a053aab3892c041272ce0e7e675a
Dan Wendorf <dan.wendorf@opendoor.com>
2018-05-20 12:03:56 -0700

    Can set value by stdin or interactively

--- cmd/set.go
+++ cmd/set.go
@@ -1,13 +1,16 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"syscall"
 	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/wendorf/dory/client"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func init() {
@@ -16,14 +19,13 @@ func init() {
 	setCommand.Flags().StringP("value", "v", "", "Value of memory")
 	setCommand.Flags().DurationP("duration", "d", time.Nanosecond, "Lifetime of memory (e.g. 1h5m2s)")
 	setCommand.MarkFlagRequired("name")
-	setCommand.MarkFlagRequired("value")
 	setCommand.MarkFlagRequired("duration")
 }
 
 var setCommand = &cobra.Command{
 	Use:   "set",
 	Short: "Set a memory",
-	Long:  "Set a memory",
+	Long:  "Set a memory. Supports piping value through stdin, or interactively asks if no value is provided.",
 	Run: func(cmd *cobra.Command, args []string) {
 		duration, err := cmd.Flags().GetDuration("duration")
 		if err != nil {
@@ -35,9 +37,27 @@ var setCommand = &cobra.Command{
 			log.Fatal(err)
 		}
 
-		value, err := cmd.Flags().GetString("value")
-		if err != nil {
-			log.Fatal(err)
+		var value string
+		if cmd.Flags().Changed("value") {
+			value, err = cmd.Flags().GetString("value")
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else if !terminal.IsTerminal(int(syscall.Stdin)) {
+			value = ""
+			s := bufio.NewScanner(os.Stdin)
+			for s.Scan() {
+				value+=s.Text()
+			}
+		} else {
+			fmt.Print("Enter value: ")
+			byteValue, err := terminal.ReadPassword(int(syscall.Stdin))
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Print("\n")
+			value = string(byteValue)
 		}
 
 		socketPath, ok := os.LookupEnv("DORY_SOCK")
